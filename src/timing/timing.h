0456931340113e7c7a895c2e434d09dababa8a68
arondlittle <littleja@my.erau.edu>
2026-07-25 17:57:59 -0500

    Refactor IntervalTimerUpdate and add TimingDelayUntil

--- src/timing/timing.h
+++ src/timing/timing.h
@@ -1,17 +1,13 @@
 #pragma once
 #include <cstdint>
 
-// Timing instrumentation helpers (pure functions, no I/O).
 struct IntervalTimer
 {
-	uint32_t lastTick = 0;
-	bool primed = false;
+    uint32_t lastTick = 0;
+    bool primed = false;
 };
 
-// Elapsed ticks since the previous call (0 on the first call).
-uint32_t IntervalTimerUpdate(IntervalTimer& t, uint32_t nowTick);
-
-// actual - expected (+ = late, - = early).
+uint32_t IntervalTimerUpdate(IntervalTimer& timer, uint32_t nowTick);
 int32_t TimingJitter(uint32_t actualInterval, uint32_t expectedPeriod);
-
 uint32_t TimingElapsed(uint32_t startTick, uint32_t nowTick);
+uint32_t TimingDelayUntil(uint32_t& nextReleaseTick, uint32_t periodMs);
