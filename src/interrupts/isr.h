e6c9d2d28d8619a52d74489883ae05b0544358cc
arondlittle <littleja@my.erau.edu>
2026-07-25 14:46:49 -0500

    Modify ISR header for interrupt handling functions
    Commented out existing function declarations and added new ones for interrupt handling.

--- src/interrupts/isr.h
+++ src/interrupts/isr.h
@@ -1,3 +1,18 @@
-#pragma once
+//#pragma once
 
-void isr_signal(int latency);
+//void isr_signal(int latency);
+
+#pragma once  
+//Tyler M7 Change
+
+// Creates the ISR-to-task semaphore.
+bool InterruptSystemInit();
+
+// Deletes the ISR-to-task semaphore.
+void InterruptSystemShutdown();
+
+// Independent simulated asynchronous event source.
+void SimulatedEventSourceTask(void *params);
+
+// Task that performs meaningful event processing.
+void InterruptEventHandlerTask(void *params);
