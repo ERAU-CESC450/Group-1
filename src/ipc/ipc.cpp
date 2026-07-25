9d60d7b7113fc8dbb7374d572f583bfac6412c87
arondlittle <littleja@my.erau.edu>
2026-07-25 17:36:58 -0500

    Fix formatting and parameter references in ipc.cpp

--- src/ipc/ipc.cpp
+++ src/ipc/ipc.cpp
@@ -3,26 +3,27 @@
 
 namespace
 {
-	QueueHandle_t g_queue = nullptr;
+    QueueHandle_t g_queue = nullptr;
 }
 
-void IpcInit(uint32_t queueLength)
+int IpcInit(uint32_t queueLength)
 {
-	g_queue = xQueueCreate(queueLength, sizeof(SystemMessage));
+    g_queue = xQueueCreate(queueLength, sizeof(SystemMessage));
+    return g_queue ? 1 : 0;
 }
 
 void IpcShutdown()
 {
-	vQueueDelete(g_queue);
-	g_queue = nullptr;
+    vQueueDelete(g_queue);
+    g_queue = nullptr;
 }
 
-int IpcSend(const SystemMessage &msg, uint32_t timeoutMs)
+int IpcSend(const SystemMessage& message, uint32_t timeoutMs)
 {
-	return xQueueSend(g_queue, &msg, timeoutMs);
+    return xQueueSend(g_queue, &message, timeoutMs);
 }
 
-int IpcReceive(SystemMessage &out, uint32_t timeoutMs)
+int IpcReceive(SystemMessage& out, uint32_t timeoutMs)
 {
-	return xQueueReceive(g_queue, &out, timeoutMs);
+    return xQueueReceive(g_queue, &out, timeoutMs);
 }
